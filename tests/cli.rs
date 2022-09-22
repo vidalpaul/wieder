fad5e597b2c43c4bf4ef8a464e14b54d48e43182
Paulo Vidal <u1d4lp@gmail.com>
2022-09-21 21:06:54 -0300

    add success test

--- tests/cli.rs
+++ tests/cli.rs
@@ -32,3 +32,9 @@ fn dies_invalid_repeat() {
         .failure()
         .stderr(predicate::str::contains("USAGE"));
 }
+
+#[test]
+fn runs() {
+ let mut cmd = Command::cargo_bin("wieder").unwrap();
+ cmd.arg("hello").assert().success();
+}
